5e50e992ee17b163f25e3cbc30ea2f7d9c1e7f11
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-25 20:17:58 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("-- Projétil --\n");
+
+Console.Write("Entre com a velocidade, em m/s..: ");
+Double velocidadeInicial = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entre com o ângulo, em graus....: ");
+double AnguloGrau = Convert.ToDouble(Console.ReadLine());
+
+double AnguloRadiano = AnguloGrau * (Math.PI / 180);
+const double Gravidade = 9.80665;
+
+double altura = (Math.Pow(velocidadeInicial * Math.Sin(AnguloRadiano), 2)) / (2 * Gravidade);
+double alcance = (Math.Pow(velocidadeInicial, 2) * Math.Sin(2 * AnguloRadiano)) / Gravidade;
+
+Console.WriteLine($"\nAlcance........: {alcance:N2} m");
+Console.WriteLine($"Altura máxima..: {altura:N2} m");
